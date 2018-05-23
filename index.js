34f08ae053f499a4d4f9299051af5d4e8a176499
Eric Choo <eric.choo1997@yahoo.com>
2018-05-23 23:53:26 +0800

    add white and blue

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+* @author Eric Choo
+* @license : MIT
+*/
+
+// white - gray - black
+export const white = '#FFFFFF'
+export const ghostwhite = '#F8F8FF'
+export const whitesmoke = '#F5F5F5'
+export const gainsboro = '#DCDCDC'
+export const lightgray = '#D3D3D3'
+export const silver = '#C0C0C0'
+export const darkgray = '#A9A9A9'
+export const lightslategray = '#778899'
+export const slategray = '#708090'
+export const gray = '#808080'
+export const dimgray = '#696969'
+export const black = '#000000'
+
+// blues
+export const cyan = '#00FFFF'
+export const aliceblue = '#F0F8FF'
+export const azure = '#F0FFFF'
+export const lightcyan = '#E0FFFF'
+export const paleturquoise = '#AFEEEE'
+export const powderblue = '#B0E0E6'
+export const skyblue = '#87CECB'
+export const turquoise = '#40E0D0'
+export const mediumturquoise = '#48D1CC'
+export const darkturquoise = '#00CED1'
+export const darkcyan = '#008B8B'
+export const steelblue = '#4682B4'
+export const lightblue = '#ADD8E6'
+export const lightsteelblue = '#B0C4DE'
+export const lightskyblue = '#87CEFA'
+export const deepskyblue = '#00BFFF'
+export const cornflowerblue = '#6495ED'
+export const dodgerblue = '#1E90FF'
+export const royalblue = '#4169E1'
+export const mediumblue = '#0000CD'
+export const darkslateblue = '#483D8B'
+export const darkblue = '#00008B'
+export const navy = '#000080'
+export const midnightblue = '#191970'
